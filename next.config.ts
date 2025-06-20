b42e9e4a347ad23463a842d542c52a76d95d4da8
spite400k <spite400k@gmail.com>
2025-06-21 01:44:28 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
 };
-module.exports = {
-  experimental: {
-    appDir: true,
-  },
-}
+
 export default nextConfig;
